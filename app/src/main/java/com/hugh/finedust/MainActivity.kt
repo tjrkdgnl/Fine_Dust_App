995128af2b407cad0d84513bb4676f916b87eec0
tjrkdgnl <skh0818@hanyang.ac.kr>
2022-12-12 20:40:03 +0900

    Initial commit

---
+++ app/src/main/java/com/hugh/finedust/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hugh.finedust
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
